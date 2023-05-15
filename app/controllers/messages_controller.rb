71b503dd53503c2dc0dae18c61600e90ae721e48
Meenoow <100647073+Meenoow@users.noreply.github.com>
2023-05-15 14:33:34 +0000

    Making ChatGPT work

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,98 +18,22 @@ class MessagesController < ApplicationController
     user_message.save
 
     # use the OpenAI API to generate a response to the user's input
-    # response = OpenAI::GPT.new(
-    #   access_token: ENV.fetch("OPENAI_TOKEN"),
-    #   model: "text-davinci-002",
-    #   prompt: user_message.content,
-    #   max_tokens: 150,
-    #   temperature: 0.5
-    # ).complete
+    response = @current_user.chat(
+      parameters: {
+          model: ENV.fetch("OPENAI_TOKEN"),
+          messages: api_messages_array,
+          temperature: 1.0,
+  })
 
-    # # create a new message with the AI's response
-    # ai_message = Message.new
-    # ai_message.role = "work-bud"
-    # ai_message.content = response.choices.first.text
+    # create a new message with the AI's response
+    ai_message = @current_user.messages.new
+    ai_message.role = "work-bud"
+    ai_message.content = response.choices.first.text
 
     # save the message to the database
-    # ai_message.save
+    ai_message.save
 
     # redirect to the chat page
     redirect_to("/messages")
   end
 end
-
-##old code
-=begin class MessagesController < ApplicationController
-  def create
-    the_message = Message.new
-    the_message.role = params.fetch("query_role")
-    the_message.content = params.fetch("query_content")
-
-    system_message = Message.new
-    system_message.role = "system"
-    system_message.content = "You are an experience teacher. Ask the user five questions to assess their #{message.topic} proficiency. Start with an easy question. After each answer, increase or decrease the difficulty of the next question based on how well the user answered.
-    In the end, provide a score between 0 and 10."
-    system_message.save
-
-    if the_message.valid?
-      the_message.save
-
-      client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_TOKEN"))
-
-      api_messages_array = Array.new
-
-        api_messages_array.push(message_hash)
-      end
-
-      response = client.chat(
-        parameters: {
-          model: ENV.fetch("OPENAI_TOKEN"),
-          messages: api_messages_array,
-          temperature: 1.0,
-        },
-      )
-## the student writes the message ##
-      user_message = Message.new
-      user_message.role = "@current_user.username"
-      user_message.content = "Please answer #{message.content}"
-      user_message.save
-## the assistant responds to the message ##
-      assistant_message = Message.new
-      assistant_message.role = "assistant"
-      assistant_message.content = response.fetch("choices").at(0).fetch("message").fetch("content")
-      assistant_message.save
-
-## new message code create ##
-
-    client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_TOKEN"))
-
-    api_messages_array = Array.new
-
-    ## if not quiz then question table? ##
-
-    quiz_messages = Message.where({ :quiz_id => the_quiz.id }).order(:created_at)
-
-    quiz_messages.each do |the_message|
-      message_hash = { :role => the_message.role, :content => the_message.content }
-
-      api_messages_array.push(message_hash)
-    end
-
-    response = client.chat(
-      parameters: {
-          model: ENV.fetch("OPENAI_TOKEN"),
-          messages: api_messages_array,
-          temperature: 1.0,
-      }
-    )
-
-    assistant_message = Message.new
-    assistant_message.role = "Work-Bud"
-    assistant_message.content = response.fetch("choices").at(0).fetch("message").fetch("content")
-    assistant_message.save
-
-    redirect_to("/todo/index")
-  end
-end
-=end
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -27,11 +27,11 @@ module FinalProject
       g.test_framework nil
       g.factory_bot false
       g.scaffold_stylesheet false
-      g.stylesheets     false
-      g.javascripts     false
-      g.helper          false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
     end
-    
+
     config.action_controller.default_protect_from_forgery = false
     config.active_record.belongs_to_required_by_default = false
 
@@ -48,7 +48,29 @@ module FinalProject
       ActionDispatch::Static,
       Rails.root.join("docs").to_s,
       index: config.public_file_server.index_name,
-      headers: config.public_file_server.headers || {}
+      headers: config.public_file_server.headers || {},
     )
+    config.before_configuration do
+      ENV["OPENAI_TOKEN"] = ""
+      puts "OPENAI_TOKEN = #{ENV["OPENAI_TOKEN"]}"
+      #KeyError - key not found: "OPENAI_TOKEN":
+      #app/controllers/messages_controller.rb:23:in `create'
+      #Started POST "/__better_errors/525694dfdb775a99/variables" for 98.206.51.40 at 2023-05-13 17:00:46 +0000
+      
+=begin this is what I see on the terminal
+  User Load (0.1ms)  SELECT "users".* FROM "users" WHERE "users"."id" = ? ORDER BY "users"."id" ASC LIMIT ?  [["id", 2], ["LIMIT", 1]]
+  ↳ app/controllers/application_controller.rb:10:in `load_current_user'
+   (0.1ms)  begin transaction
+  ↳ app/controllers/messages_controller.rb:18:in `create'
+  User Load (0.1ms)  SELECT "users".* FROM "users" WHERE "users"."id" = ? LIMIT ?  [["id", 2], ["LIMIT", 1]]
+  ↳ app/controllers/messages_controller.rb:18:in `create'
+  Message Create (0.9ms)  INSERT INTO "messages" ("content", "role", "user_id", "created_at", "updated_at") VALUES (?, ?, ?, ?, ?)  [["content", "What is an adjective?"], ["role", "user"], ["user_id", 2], ["created_at", "2023-05-13 17:00:45.804693"], ["updated_at", "2023-05-13 17:00:45.804693"]]
+  ↳ app/controllers/messages_controller.rb:18:in `create'
+   (33.5ms)  commit transaction
+  ↳ app/controllers/messages_controller.rb:18:in `create'
+Completed 500 Internal Server Error in 49ms (ActiveRecord: 34.7ms | Allocations: 7791)
+=end
+    end
   end
+
 end
